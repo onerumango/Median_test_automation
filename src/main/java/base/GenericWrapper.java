bd5677f733d36acfe7fac7e4671391552bbbba51
Veeresh Badiger <38921254+Veereshbadiger@users.noreply.github.com>
2022-05-16 15:23:04 +0530

    updated

---
+++ src/main/java/base/GenericWrapper.java
@@ -0,0 +1,29 @@
+package base;
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class GenericWrapper {
+
+}
---
+++ src/main/java/testcases/MedianLoginTestcase0001.java
@@ -0,0 +1,5 @@
+package testcases;
+
+public class MedianLoginTestcase0001 {
+
+}
