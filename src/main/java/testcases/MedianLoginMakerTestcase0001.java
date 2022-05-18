ad17a4910e5d74f94b969ec2d79a7236aa357e4f
Veeresh Badiger <38921254+Veereshbadiger@users.noreply.github.com>
2022-05-18 16:07:25 +0530

    TestNG_configured

--- src/main/java/testcases/MedianLoginMakerTestcase0001.java
+++ src/main/java/testcases/MedianLoginMakerTestcase0001.java
@@ -2,11 +2,13 @@ package testcases;
 
 
 
+import org.testng.annotations.Test;
+
 import base.MedianSpecificMethods;
 import pages.MedianLogin;
 
 public class MedianLoginMakerTestcase0001 extends MedianSpecificMethods {
-	
+	@Test
 	public void runMedianLogin() {
 		MedianLogin login = new MedianLogin();
 		login.enterUsername().enterPassword().clickSignInButton();
