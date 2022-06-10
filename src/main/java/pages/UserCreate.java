267fbecf2e7817c6d4b90a9d6a1db2f5247f3403
VikasRumango <80444549+VikasRumango@users.noreply.github.com>
2022-06-10 16:04:12 +0530

    CreateUserWorkingCode

--- src/main/java/pages/UserCreate.java
+++ src/main/java/pages/UserCreate.java
@@ -7,11 +7,11 @@ public class UserCreate extends MedianSpecificMethods{
 	
 	
       public UserCreate enterUserId() {
-    	  enterValue(locateElement("xpath", props.getProperty("Median.SystemMaintenance.Users.New.UserID.Xpath")), props.getProperty("User Id"));
+    	  enterValue(locateElement("xpath", props.getProperty("Median.SystemMaintenance.Users.New.UserID.Xpath")), props.getProperty("UserID"));
   		return this;
       }
       public UserCreate enterUserName() {
-    	  enterValue(locateElement("xpath", props.getProperty("Median.SystemMaintenance.Users.New.Username.Xpath")), props.getProperty("User Name"));
+    	  enterValue(locateElement("xpath", props.getProperty("Median.SystemMaintenance.Users.New.Username.Xpath")), props.getProperty("UserName"));
   		return this;
       }
       public UserCreate enterPassword() {
@@ -29,13 +29,21 @@ public class UserCreate extends MedianSpecificMethods{
       }
       
       public UserCreate selectRolesForUSer() {
+    	wait(2000);
+    	jsExecutor(locateElement("xpath", props.getProperty("Median.SystemMaintenance.Users.New.Save.Xpath")));
+    	wait(1000);
   		clickElement(locateElement("xpath", props.getProperty("Median.SystemMaintenance.Users.New.RolesForUser.Dropdown.Xpath")));
-  		wait(1000);
-  		clickElement(locateElement("xpath", props.getProperty("Median.SystemMaintenance.Users.New.RolesForUser.Dropdown.Demorole.Xpath")));
+  		wait(2000);
+  		enterValue(locateElement("xpath", props.getProperty("Median.SystemMaintenance.Users.New.RolesForUser.DropdownSearch.Xpath")), props.getProperty("DropdownSearch"));
+  		wait(2000);
+  		clickElement(locateElement("xpath", props.getProperty("Median.SystemMaintenance.Users.New.RolesForUser.Dropdown.SelectRole.Xpath")));
   		return this;
   	}
       
       public UserCreate selectDepartment() {
+    	  wait(1000);
+    	  jsExecutor(locateElement("xpath", props.getProperty("Median.SystemMaintenance.Users.New.Department.Dropdown.Xpath")));
+    	wait(1000);
   		clickElement(locateElement("xpath", props.getProperty("Median.SystemMaintenance.Users.New.Department.Dropdown.Xpath")));
   		wait(1000);
   		clickElement(locateElement("xpath", props.getProperty("Median.SystemMaintenance.Users.New.Department.Dropdown.Option.Xpath")));
@@ -43,6 +51,9 @@ public class UserCreate extends MedianSpecificMethods{
   	}
       
       public UserCreate clickSave() {
+    	  wait(1000);
+    	  jsExecutor(locateElement("xpath", props.getProperty("Median.SystemMaintenance.Users.New.Save.Xpath")));
+    	  wait(5000);
     	  clickElement(locateElement("xpath", props.getProperty("Median.SystemMaintenance.Users.New.Save.Xpath")));
     	  return this;
       }
