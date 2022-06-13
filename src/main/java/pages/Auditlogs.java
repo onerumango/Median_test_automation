0775fb535ed1bf3e31ca0e1c7864609eb70929a8
VikasRumango <80444549+VikasRumango@users.noreply.github.com>
2022-06-13 17:33:04 +0530

    working code

--- src/main/java/pages/Auditlogs.java
+++ src/main/java/pages/Auditlogs.java
@@ -5,9 +5,13 @@ import base.MedianSpecificMethods;
 public class Auditlogs extends MedianSpecificMethods{
 	
       public Auditlogs selectform() {
+    	  wait(1000);
     		clickElement(locateElement("xpath", props.getProperty("Median.SystemMaintenance.Auditlogs.form.Dropdown.Xpath")));
     		wait(1000);
     		clickElement(locateElement("xpath", props.getProperty("Median.SystemMaintenance.Auditlogs.form.Dropdown.Pdf.Xpath")));
+    		wait(1000);
+    		clickElement(locateElement("xpath", props.getProperty("Median.SystemMaintenance.Auditlogs.form.DownloadButton.Xpath")));
+    		wait(5000);
     		return this;
     	}
 }
