e28a463837f26e3931b2e6403914fe58865ef7ef
VikasRumango <80444549+VikasRumango@users.noreply.github.com>
2022-06-15 17:55:44 +0530

    working code

--- src/main/java/pages/EmailManagementSummary.java
+++ src/main/java/pages/EmailManagementSummary.java
@@ -4,14 +4,17 @@ import base.MedianSpecificMethods;
 
 public class EmailManagementSummary extends MedianSpecificMethods{
 	public EmailManagementCreate clickNew() {
+		wait(2000);
 		clickElement(locateElement("xpath", props.getProperty("EmailManagementSummary.New.Xpath")));
 			return new EmailManagementCreate();
 		}
 	public EmailmanagementEdit selectRecord() {
+		wait(2000);
 		clickElement(locateElement("xpath", props.getProperty("EmailManagementSummary.Record.Xpath")));
 		return new EmailmanagementEdit();
 	}
 	public EmailManagementAuth selectUnauthRecord() {
+		wait(2000);
 		clickElement(locateElement("xpath", props.getProperty("EmailManagementSummary.UnauthRecord.Xpath")));
 		return new EmailManagementAuth();
 	}
--- src/main/java/pages/EmailmanagementEdit.java
+++ src/main/java/pages/EmailmanagementEdit.java
@@ -5,16 +5,23 @@ import base.MedianSpecificMethods;
 public class EmailmanagementEdit extends MedianSpecificMethods{
 	
 	public EmailmanagementEdit clickEdit() {
+		wait(2000);
 		clickElement(locateElement("xpath", props.getProperty("EmailmanagementEdit.Edit.Xpath")));
 		return this;
 	}
 	public EmailmanagementEdit editEmailid() {
-		  enterValue(locateElement("xpath", props.getProperty("EmailManagementCreate.EditEmailId.Xpath")), props.getProperty("EmailId"));
+		wait(2000);
+		clearElement(locateElement("xpath", props.getProperty("EmailManagementCreate.EditEmailId.Xpath")));
+		wait(2000);
+		  enterValue(locateElement("xpath", props.getProperty("EmailManagementCreate.EditEmailId.Xpath")), props.getProperty("EmailId1"));
 			return this;
 	    }
 	
 	public EmailmanagementEdit clickSave() {
+		wait(2000);
 		clickElement(locateElement("xpath", props.getProperty("EmailmanagementEdit.Save.Xpath")));
+		wait(2000);
+		clickElement(locateElement("xpath", props.getProperty("EmailmanagementEdit.OK.Xpath")));
 		return this;
 	}
 		
