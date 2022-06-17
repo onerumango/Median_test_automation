7e9f29a89c40a9e509304923f814030329ae3def
VikasRumango <80444549+VikasRumango@users.noreply.github.com>
2022-06-17 17:30:28 +0530

    RuleTranslation

--- src/main/java/testcases/MedianRuleTranslationXMLTC0006.java
+++ src/main/java/testcases/MedianRuleTranslationXMLTC0006.java
@@ -1,19 +1,24 @@
 package testcases;
 
+import java.awt.AWTException;
+
 import org.testng.annotations.Test;
 
 import pages.MedianLoginAndLogout;
 
 public class MedianRuleTranslationXMLTC0006 {
 	@Test
-	public void runMedianRuleTranslation() throws InterruptedException {
+	public void runMedianRuleTranslation() throws InterruptedException, AWTException {
 		MedianLoginAndLogout login = new MedianLoginAndLogout();
 		login.enterMakerUsername().enterMakerPassword().clickSignInButton()
 		.clickSystemInterface()
 	    .clickRuleTranslationXML()
 	    .clickNewButton()
 	    .dropDownSourceSyetem()
-	    .clickFetchButton()
+	    .fileUpload()
+	    .selectTag()
+	    .selectDataType()
+	    
 	    .clickSaveButton()
 		.clickOkPopup();
 	}
