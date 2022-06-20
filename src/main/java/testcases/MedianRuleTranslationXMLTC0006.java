3d16d01185aac0bc4ba5fd8df93aa77a0a28a95e
VikasRumango <80444549+VikasRumango@users.noreply.github.com>
2022-06-20 15:33:27 +0530

    updatedCode

--- src/main/java/testcases/MedianRuleTranslationXMLTC0006.java
+++ src/main/java/testcases/MedianRuleTranslationXMLTC0006.java
@@ -8,9 +8,11 @@ import pages.MedianLoginAndLogout;
 
 public class MedianRuleTranslationXMLTC0006 {
 	@Test
-	public void runMedianRuleTranslation() throws InterruptedException, AWTException {
+	public void runMedianLogin() throws InterruptedException, AWTException {
 		MedianLoginAndLogout login = new MedianLoginAndLogout();
-		login.enterMakerUsername().enterMakerPassword().clickSignInButton()
+		login.enterMakerUsername()
+		.enterMakerPassword()
+		.clickSignInButton()
 		.clickSystemInterface()
 	    .clickRuleTranslationXML()
 	    .clickNewButton()
@@ -18,7 +20,9 @@ public class MedianRuleTranslationXMLTC0006 {
 	    .fileUpload()
 	    .selectTag()
 	    .selectDataType()
-	    
+	    .clickPlusButton()
+	    .selectTag2()
+	    .selectDataType2()
 	    .clickSaveButton()
 		.clickOkPopup();
 	}
